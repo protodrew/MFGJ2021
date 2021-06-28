bdab47234200315470c71ef950884f3c7c3d5ff2
Drew <protodrew@pop-os.localdomain>
2021-06-28 04:24:39 +0200

    cleaned up script and added placeholder art

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-
-var MAX_SPEED = 100
-var ACCELERATION = 400
-var FRICTION = 100
+const MAX_SPEED = 200
+var ACCELERATION = 80
+var FRICTION = 80
 var velocity = Vector2.ZERO # initializes an empty Vector2 that will store our X and Y velocity.
-var last_input = Vector2.ZERO
+
 
 # Called Every Tick that the Physics Updates
 func _physics_process(delta):
+
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # fluctuates between -1 and 1 with 0 being the resting place, stored in Vector2 to become velocity later
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")    # <- fluctuates between -1 and 1 with 0 being the resting place, stored in Vector2 to become velocity later
-	input_vector = input_vector.normalized()
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # fluctuates between -1 and 1 with 0 being the resting place, stored in Vector2 to become xvelocity later
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")    # <- fluctuates between -1 and 1 with 0 being the resting place, stored in Vector2 to become yvelocity later
+	input_vector = input_vector.normalized() # makes it so that moving diagonally isn't faster than moving in one of the 4 cardinal directions
 	
 	
 	if input_vector != Vector2.ZERO: #checks we are giving an input
@@ -21,3 +21,4 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION) # moves velocity to zero incrementing by the FRICTION
 		
 	velocity = move_and_slide(velocity) # used for kinematic bodies, moves the KinematicBody2D and stops if a collision occurs multiplied by delta so framerate doesn't affect 
+
